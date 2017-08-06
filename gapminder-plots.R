9de8950f01bc23df0492beb48a9d3ec68c65ce1a
Miranda Stratton <miranda.stratton8@gmail.com>
2017-08-06 10:04:05 -0700

    add gdppercap vs lifeexp plot

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,4 +1,3 @@
-
 #Given that this is the same data as yesterday, we could open it up in its directory
 #however, you would have to open it in GIT and make sure all the directories are aligned properly
 #easiest for class to download the file again 
@@ -6,3 +5,8 @@
 
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv","data/gapminder-FiveYears.csv")
 gapminder <- read.csv("data/gapminder-FiveYears.csv")
+
+library(ggplot2)
+
+ggplot(gapminder,aes(y = lifeExp, x = gdpPercap)) +
+  geom_point()
